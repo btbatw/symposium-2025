57126f77f2f67cc863e8fcdc13ca1510125a761f
Yu-An Chen <atwood12@gmail.com>
2024-07-14 00:43:29 -0700

    Use lodash `orderBy` to sort speakers with custom sorting props

--- client/src/app/website/website.service.ts
+++ client/src/app/website/website.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 // import { Http, Response } from '@angular/http';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { orderBy } from "lodash";
 
 
 import { Observable } from 'rxjs/Observable';
@@ -58,14 +59,13 @@ export class WebsiteService {
 				element.speakers = parsedSpeakers;
 				allSpeakers = [...allSpeakers, parsedSpeakers];
 			});
-		let lunchSpeakers = allSpeakers
-			.reduce((prev, curr) => [...prev, ...curr], [])
-			.filter(speaker => speaker.title === "Lunch & Round Table with Speakers");
-		let nonLunchSpeakers = allSpeakers
-			.reduce((prev, curr) => [...prev, ...curr], [])
-			.filter(speaker => speaker.title !== "Lunch & Round Table with Speakers");
 
-		this.speakers = [...nonLunchSpeakers, ...lunchSpeakers]
+		allSpeakers = allSpeakers.reduce((prev, curr) => [...prev, ...curr], [])
+
+		let nonLunchSpeakers = allSpeakers
+			.filter(speaker => !speaker.title.includes('Lunch & Round Table with Speakers') && !speaker.title.includes('Breakfast and Meeting with Experts'));
+
+		allSpeakers = [...nonLunchSpeakers, ...allSpeakers]
 			// only keep unique items
 			.reduce((prev, curr) => {
 				return prev.length === 0
@@ -74,41 +74,18 @@ export class WebsiteService {
 					? [...prev]
 					: [...prev, ...curr];
 			}, [])
-			// keynote at front and sort by session time
-			.sort((a, b) => {
-				return a.title.includes('Keynote')
-					? -1
-					: 1
-			})
-			.sort((a, b) => {
-				return a.title.includes('Keynote') || b.title.includes('Keynote')
-					? 0
-					: new Date(a.from).getTime() - new Date(b.from).getTime()
-			})
-			// sort parallel session by session title
-			.sort((a, b) => {
-				return new Date(a.from).getTime() !== new Date(b.from).getTime()
-					? 0
-					: a.title.toUpperCase() > b.title.toUpperCase()
-					? 1
-					: -1;
-			})
-			// sort by last name
-			.sort((a, b) => {
-				return a.title.includes('Keynote') || b.title.includes('Keynote')
-					? 0
-					: a.title.toUpperCase() !== b.title.toUpperCase()
-					? 0
-					: a.name.split(' ').pop().toUpperCase() > b.name.split(' ').pop().toUpperCase()
-					? 1
-					: -1;
-			})
-			// sort keynote by session time
-			.sort((a, b) => {
-				return a.title.includes('Keynote') && b.title.includes('Keynote')
-					? new Date(a.from).getTime() - new Date(b.from).getTime()
-					: 0;
-			});
+			.map(speaker => ({
+				...speaker,
+				lastName: speaker.name.split(' ').pop().toUpperCase(),
+				isKeynote: speaker.title.toLowerCase().includes('keynote'),
+				isSpecialTalk: speaker.title.toLowerCase().includes('special talk'),
+			}));
+
+		this.speakers = orderBy(
+			allSpeakers,
+			['isKeynote', 'isSpecialTalk', 'from', 'title', 'lastName'],
+			['desc', 'desc', 'asc', 'asc', 'asc']
+		);
 	}
 
 	private parseInfoSessionSpeakersGroups(data) {
